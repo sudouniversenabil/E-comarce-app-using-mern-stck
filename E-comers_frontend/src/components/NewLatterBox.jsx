d46ef2fbc9d8d7314a22798469c71452179d97d8
sudouniversenabil <nabilsikder00@gmail.com>
2026-08-17 09:45:18 +0600

    i dont

---
+++ E-comers_frontend/src/components/NewLatterBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function NewLatterBox() {
+
+    // and also dnot now what is preen defult do 
+    const onSubmitDefault=(event)=>{
+        event.prevenDefault()
+    }
+  return (
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-700 '> subcibe now and get 20& off discount</p>
+
+        <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt voluptatum commodi, nesciunt eligendi dolor rem mollitia minus expedita, in magnam necessitatibus minima accusantium explicabo sapiente eveniet nemo quasi cum illo.</p>
+
+        {/* i dont know what is onsubmit do */}
+        <form onSubmit={onSubmitDefault} className='w-full sm:w-1/2 flex items-center gap-3 my-6 border pl-3'>
+            <input className='w-full sm:flex-1 outline-none' type="text" placeholder='Enter your email' required />
+            <button type='Submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
+        </form>
+    </div>
+  )
+}
+
+export default NewLatterBox
--- E-comers_frontend/src/pages/Home.jsx
+++ E-comers_frontend/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewLatterBox from '../components/NewLatterBox'
 function Home() {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
       <BestSeller/>
+      <OurPolicy/>
+      <NewLatterBox/>
     </div>
   )
 }
