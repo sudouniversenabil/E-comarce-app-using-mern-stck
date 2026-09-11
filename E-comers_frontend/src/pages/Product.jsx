81f63a314c8e065a88c10fc6a5a27ff2af2c37d6
sudouniversenabil <nabilsikder00@gmail.com>
2026-09-11 21:33:21 +0600

    date

--- E-comers_frontend/src/pages/Product.jsx
+++ E-comers_frontend/src/pages/Product.jsx
@@ -1,15 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/Shopcontext'
-import { products } from '../assets/assets'
-
+import { assets } from '../assets/assets'
 const Product = () => {
   const { productId } = useParams()  // what is the use parms i need more explin
-  const { Product } = useContext(ShopContext)
+  const { products,currency } = useContext(ShopContext)
   const [productdata, setproductdata] = useState(false)
   const [image, seImage] = useState('')
+  const [size,setSize]=useState("")
   const fetchProductData = async () => {
-    Product.map((item) => {
+    products.map((item) => {
       if (item._id === productId) {
         setproductdata(item)
         seImage(item.image[0])
@@ -18,6 +18,7 @@ const Product = () => {
     })
   }
   useEffect(() => {
+    console.log(productdata)
     fetchProductData()
   }, [productdata])
   console.log(productId)
@@ -28,12 +29,38 @@ const Product = () => {
         {/* product iamges */}
         <div className='flex-1 flex flex-col gap-2 sm:flex-row'>
           <div className='flex sm:flex-col overflow-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
-{
-  productdata.image.map((item,index)=>(
-<img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 	
+            {
+              productdata.image.map((item, index) => (
+                <img onClick={() => seImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 	
 flex-shrink:0 cursor-pointer' alt="" />
-  ))
-}
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-4/5'>
+            <img className='w-[50%] h-au' src={image} alt="" />
+          </div>
+        </div>
+        {/* =========prodct info============ */}
+        <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2'>{productdata.name}</h1>
+          <div className='flex items-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+
+          </div>
+          <p className='mt-5 text-2xl font-medium'> {currency}{productdata.price}</p>
+          <p className='mt-5 text-gray-500 w-[80%]'>{productdata.description}</p>
+
+          <div className='flex flex-col gap-4 my my-8'>
+            <p>Select Size</p>
+            <div className='flex gap-2'>
+              { productdata.sizes.map((item,index)=>(
+                <button onClick={()=> setSize(item)} className={`cursor-pointer border py-2 px-4  bg-gray-150 ${size===item ? 'border-amber-600 ' :""}`}key={index}> {item}</button>
+              ))  }
+            </div>
           </div>
         </div>
       </div>
